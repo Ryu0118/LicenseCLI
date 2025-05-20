63ee5be162e5198409e01d9e78856da5c563c811
Ryu0118 <ryunosuke.shibuya.3d@stu.hosei.ac.jp>
2025-05-21 01:01:51 +0900

    Update IntegrationTests to validate generated Licenses are all correct

--- Tests/LicenseCLITests/IntegrationTests.swift
+++ Tests/LicenseCLITests/IntegrationTests.swift
@@ -9,9 +9,32 @@ final class IntegrationTests{
     let outputFileURL: URL
     let tcaFixtureURL: URL
     let nioFixtureURL: URL
+    let printLicensesURL: URL
+    let binaryOutputURL: URL
     let fileManager: FileManager
 
     let outputFileName = "Licenses"
+
+    let nioAndTcaDependencies: Set<String> = [
+        "swift-atomics",
+        "swift-collections",
+        "swift-nio",
+        "swift-system",
+        "combine-schedulers",
+        "swift-case-paths",
+        "swift-clocks",
+        "swift-collections",
+        "swift-composable-architecture",
+        "swift-concurrency-extras",
+        "swift-custom-dump",
+        "swift-dependencies",
+        "swift-identified-collections",
+        "swift-navigation",
+        "swift-perception",
+        "swift-sharing",
+        "swift-syntax",
+        "xctest-dynamic-overlay"
+    ]
 
     init() throws {
         let fixtureURL = URL(filePath: #filePath).deletingLastPathComponent().appending(component: "Fixtures")
@@ -20,6 +43,8 @@ final class IntegrationTests{
         fileManager = FileManager.default
         tcaFixtureURL = fixtureURL.appending(component: "ComposableArchitecture")
         nioFixtureURL = fixtureURL.appending(component: "NIO")
+        printLicensesURL = fixtureURL.appending(component: "PrintLicenses").appendingPathExtension("swift")
+        binaryOutputURL = outputDirURL.appending(component: "main")
 
         try fileManager.createDirectory(at: outputDirURL, withIntermediateDirectories: true)
     }
@@ -58,10 +83,23 @@ final class IntegrationTests{
                 launchPath: "/usr/bin/xcrun",
                 arguments: [
                     "swiftc",
-                    outputFileURL.path(percentEncoded: false)
+                    outputFileURL.path(percentEncoded: false),
+                    printLicensesURL.path(percentEncoded: false),
+                    "-o",
+                    binaryOutputURL.path(percentEncoded: false),
                 ]
             )
         }
+
+        let idsData: Data = try #require(
+            try Command.run(
+                launchPath: binaryOutputURL.path(percentEncoded: false),
+                arguments: []
+            )
+        )
+        let ids = try JSONDecoder().decode(Set<String>.self, from: idsData)
+
+        #expect(ids == nioAndTcaDependencies)
     }
 }
 
